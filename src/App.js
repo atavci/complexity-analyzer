7740fd8bc3f22e26274dfd190d668397f97ac6f5
Ali Turkay AVCI <turkayavci@gmail.com>
2023-04-02 09:01:21 +0300

    Initial version

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
 import "prismjs/themes/prism.css";
 import "./custom-prism.css";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  "https://fbjjixkrcvwcptzvtkqw.supabase.co",
+  process.env.REACT_APP_ANON_KEY
+);
+
+function findBigONotation(inputText) {
+  const regex = /O\(\s*[a-zA-Z0-9\s+\-*/^]*\)/g;
+  const matches = inputText.match(regex);
+
+  return matches || [];
+}
+
 
 function App() {
   const [code, setCode] = useState(
@@ -16,36 +30,40 @@ function App() {
   return true;
 }`
   );
-  const [results, setResults] = useState("Your complexity analysis will be displayed here.");
+  const [results, setResults] = useState(
+    "Your complexity analysis will be displayed here."
+  );
   const [loading, setLoading] = useState(false);
 
   function handleResult(results) {
+    console.log(results);
+    if (results && results.choices && results.choices.length > 0) {
+      console.log(results.choices);
+      setResults(results.choices[0].message.content.toString());
+      return true;
+    }
     setResults(results);
   }
 
   function analyzeCode() {
     setLoading(true);
-    const url = "http://localhost:3000/conversation";
-    const data = {
-      message:
-        "Calculate the complexity of the following code and output the result with big O notation. Code: " +
-        code,
-    };
-    console.log("data: " + JSON.stringify(data));
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => {
-        response.json().then((responseData) => {
-          handleResult(responseData.response);
-          setLoading(false);
-        });
+    supabase.functions
+      .invoke("complexity-analyzer", {
+        body: { prompt: code },
       })
-      .catch((error) => console.error("Error:", error));
+      .then((response) => {
+        try {
+          handleResult(response.data.results);
+        } catch (err) {
+          handleResult("Unknown issue encountered");
+        }
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        handleResult("error");
+        setLoading(false);
+      });
   }
 
   return (
@@ -56,7 +74,7 @@ function App() {
       <main>
         <section className="left-column">
           <div className="code-editor">
-          <h2>Code</h2>
+            <h2>Code</h2>
             <Editor
               lassName="code"
               value={code}
@@ -72,13 +90,21 @@ function App() {
           <p className="description">
             Paste your code above to analyze its complexity in Big O Notation.
           </p>
-          <button id="analyze-btn" onClick={analyzeCode}>Analyze</button>
+          <button id="analyze-btn" onClick={analyzeCode}>
+            Analyze
+          </button>
         </section>
         <section className="right-column">
           <div className="results">
-            <h2>Results</h2>
+            <div className="grid-container">
+              <h2 className="row-el">Complexity</h2>
+              <h2 className="row-el">{findBigONotation(results)[0] || ""}</h2>
+            </div>
+
             {loading ? (
-              <div className={`loading ${loading ? "pulse" : ""}`}>Loading...</div>
+              <div className={`loading ${loading ? "pulse" : ""}`}>
+                Loading...
+              </div>
             ) : (
               <ul>
                 <li>{results}</li>
--- src/Result.js
+++
@@ -1,7 +0,0 @@
-import React, { useState } from "react";
-
-class Car extends React.Component {
-    render() {
-      return <h2>Hi, I am a Car!</h2>;
-    }
-  }
